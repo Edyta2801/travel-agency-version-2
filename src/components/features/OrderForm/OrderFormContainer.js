8027a0065c0f94ba1fa15aa10b68c566ad181380
Edyta2801 <edyta.szarowska@interia.pl>
2019-12-28 13:59:48 +0100

    display order price with bug

--- src/components/features/OrderForm/OrderFormContainer.js
+++ src/components/features/OrderForm/OrderFormContainer.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 import OrderForm from './OrderForm';
-import { getOrderOptions } from '../../../redux/orderRedux';
+import { getOrderOptions } from '../../../redux/orderRedux.js';
 
 const mapStateToProps = state => ({
   options: getOrderOptions(state),
