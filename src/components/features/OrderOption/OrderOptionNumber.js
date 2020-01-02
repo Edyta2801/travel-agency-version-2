1cfdd80cfbfbb81efa21b0f20f253a6253fd66b6
Edyta2801 <edyta.szarowska@interia.pl>
2020-01-03 00:18:10 +0100

    modify components of order option types

--- src/components/features/OrderOption/OrderOptionNumber.js
+++ src/components/features/OrderOption/OrderOptionNumber.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './OrderOption.scss';
 
-const OrderOptionNumber = props => (
-  <div>
-    {props.name}
-
+const OrderOptionNumber = ({ currentValue, limits, setOptionValue }) => (
+  <div className={styles.number}>
+    <input
+      className={styles.inputSmall}
+      type='number'
+      value={currentValue}
+      min={limits.min}
+      max={limits.max}
+      onChange-={event => setOptionValue(event.currentTarget.value)}
+    />
   </div>
 );
 
 OrderOptionNumber.propTypes = {
   name: PropTypes.string,
+  currentValue: PropTypes.string,
+  limits: PropTypes.number,
+  setOptionValue: PropTypes.func,
 };
 
 export default OrderOptionNumber;
