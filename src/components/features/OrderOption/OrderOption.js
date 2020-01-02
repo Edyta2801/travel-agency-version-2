1c5a24c3bd121040e459eff5fb1397d615cbbe48
Edyta2801 <edyta.szarowska@interia.pl>
2020-01-02 22:24:54 +0100

    display the  type of order optioms

--- src/components/features/OrderOption/OrderOption.js
+++ src/components/features/OrderOption/OrderOption.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import styles from './OrderOption.scss';
+import OrderOptionDropdown from './OrderOptionDropdown.js';
+import OrderOptionIcons from './OrderOptionIcons.js';
+import OrderOptionNumber from './OrderOptionNumber.js';
+import OrderOptionCheckboxes from './OrderOptionCheckboxes.js';
 
+const optionTypes = {
+  dropdown: OrderOptionDropdown,
+  icons: OrderOptionIcons,
+  checkboxes: OrderOptionCheckboxes,
+  number: OrderOptionNumber,
+};
 
-
-const OrderOption = () => (
-  <div className={styles.component}>
-    <h3 className={styles.title}>{'car-rental'}</h3>
-  </div>
-);
-
+const OrderOption = ({name, type, ...otherProps}) => {
+  const OptionComponent = optionTypes[type];
+  if(!OptionComponent){
+    return null;
+  } else {
+    return (
+      <div className={styles.component}>
+        <h3 className={styles.title}>{name}</h3>
+        <OptionComponent
+          {...otherProps}
+        />
+      </div>
+    );
+  }
+};
 
 
 export default OrderOption;
