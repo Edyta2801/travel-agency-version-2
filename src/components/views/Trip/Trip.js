f2bd07a4e2e6f473071fb8266d884a0567021439
Edyta2801 <edyta.szarowska@interia.pl>
2020-01-30 23:39:32 +0100

    add changes

--- src/components/views/Trip/Trip.js
+++ src/components/views/Trip/Trip.js
@@ -15,7 +15,7 @@ import OrderForm from '../../features/OrderForm/OrderFormContainer.js';
 import styles from './Trip.scss';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 
-const Trip = ({ error, name, image, cost, days, description, country, intro }) => {
+const Trip = ({ error, name, image, cost, days, description, country, intro, id }) => {
   if (error) return <NotFound />;
   else
     return (
@@ -70,7 +70,12 @@ const Trip = ({ error, name, image, cost, days, description, country, intro }) =
           <Row>
             <Col xs={12}>
               <PageTitle text='Trip options' />
-              <OrderForm tripCost={cost} />
+              <OrderForm
+                tripCost={cost}
+                tripName={name}
+                tripId={id}
+                countryCode={country.alpha3Code}
+              />
             </Col>
           </Row>
         </Grid>
@@ -85,6 +90,8 @@ Trip.propTypes = {
   days: PropTypes.number,
   description: PropTypes.string,
   country: PropTypes.object,
+  tripId:PropTypes.number,
+  countryCode: PropTypes.object,
 };
 
 export default Trip;
